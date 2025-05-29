ed58178f5e79ec467a9adbef556167cd09f3ec84
HassanKhan123 <hk260280@gmail.com>
2025-05-29 18:48:29 +0200

    added: dynamic path segments

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the challenges index.")
+def monthly_challenge(request, month):
+    months = {
+        "january": "New Year",
+        "february": "Valentine's Day",
+        "march": "St. Patrick's Day",
+        "april": "April Fools' Day",
+        "may": "May Day",
+        "june": "Summer Solstice",
+        "july": "Independence Day",
+        "august": "Back to School",
+        "september": "Labor Day",
+        "october": "Halloween",
+        "november": "Thanksgiving",
+        "december": "Christmas"
+    }
+
+    month = month.lower()
+    if month in months:
+        return HttpResponse(f"<h1>{months[month]}</h1>")
+
+    return HttpResponseNotFound("<h1>Month not found</h1>")
